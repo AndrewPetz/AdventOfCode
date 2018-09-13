6ed2685eab88a4abc14962f0f5d447e9bffb1ed5
Andrew Petz <AnPetz@ashleyfurniture.com>
2018-09-13 08:00:38 -0500

    Finished day 2

--- AdventOfCode/AdventOfCode/Program.cs
+++ AdventOfCode/AdventOfCode/Program.cs
@@ -11,13 +11,14 @@ namespace AdventOfCode
     {
         static void Main(string[] args)
         {
-            Day1();
+            //Day1();
+            Day2();
         }
 
         private static int Day1()
         {
             int retVaL = 0;
-            string input = GetInput(1);
+            string input = GetSimpleInput(1);
             int pos = 0;
 
             //input = "(())";
@@ -41,7 +42,48 @@ namespace AdventOfCode
             return retVaL;
         }
 
-        private static string GetInput(int day)
+        private static int Day2()
+        {
+            int retVal = 0;
+            int ribbon = 0;
+            string line;
+            string filepath = Directory.GetCurrentDirectory() + "/Inputs/day2.txt";
+
+            if (File.Exists(filepath))
+            {
+                StreamReader file = new StreamReader(filepath);
+                while ((line = file.ReadLine()) != null)
+                {
+                    string[] dimensions = line.Split('x');
+                    int[] intDimensions = new int[3];
+
+                    intDimensions[0] = int.Parse(dimensions[0]);
+                    intDimensions[1] = int.Parse(dimensions[1]);
+                    intDimensions[2] = int.Parse(dimensions[2]);
+
+                    Array.Sort(intDimensions);
+
+                    int l = intDimensions[0];
+                    int w = intDimensions[1];
+                    int h = intDimensions[2];
+                    int lw = l * w;
+                    int wh = w * h;
+                    int lh = l * h;
+                    int minArea = Math.Min(lw, Math.Min(wh, lh));
+                    int vol = l * w * h;
+
+                    ribbon += vol + l + l + w + w;
+
+                    retVal += (2 * lw) + (2 * wh) + (2 * lh) + minArea;
+                }
+
+                file.Close();
+            }
+
+            return retVal;
+        }
+
+        private static string GetSimpleInput(int day)
         {
             string retVal = "";
             string filepath = Directory.GetCurrentDirectory() + "/Inputs/day" + day + ".txt";
