809351a217af14cbbda7774df45b4e3ed368b834
Andrew Petz <nannooskeeska@gmail.com>
2018-11-30 23:32:12 -0600

    Completed 2018 day 1

---
+++ 2018/AoC2018/Day01.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AoC2018
+{
+    class Day01
+    {
+        public static int Run()
+        {
+            int rVal = 0;
+
+            List<string> input = Utilities.GetLineInput("01");
+            List<int> frequencies = new List<int>();
+            
+            while (true)
+            {
+                foreach (string c in input)
+                {
+                    rVal += int.Parse(c);
+                    if (frequencies.Contains(rVal))
+                        return rVal;
+                    frequencies.Add(rVal);
+                }
+            }
+        }
+    }
+}
--- 2018/AoC2018/Program.cs
+++ 2018/AoC2018/Program.cs
@@ -10,6 +10,8 @@ namespace AoC2018
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(Day01.Run());
+            Console.ReadLine();
         }
     }
 }
