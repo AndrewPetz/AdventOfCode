c4ab07d30d2fd1741ac337c0532cd8a7c570f382
Andrew Petz <AnPetz@ashleyfurniture.com>
2018-12-05 13:38:00 -0600

    Started day 4

--- 2018/AoC2018/Day03.cs
+++ 2018/AoC2018/Day03.cs
@@ -46,5 +46,29 @@ namespace AoC2018
 
             Console.ReadLine();
         }
+
+        public static void PartTwo()
+        {
+            List<string> input = Utilities.GetLineInput("03");
+
+            int[,] fabric = new int[5000, 5000];
+
+            foreach (string s in input)
+            {
+                string[] splits = s.Split(' ');
+                int start = int.Parse(splits[2].ToString().Replace(":", "").Split(',')[0]);
+                int end = int.Parse(splits[2].ToString().Replace(":", "").Split(',')[1]);
+                int width = int.Parse(splits[3].ToString().Split('x')[0]);
+                int height = int.Parse(splits[3].ToString().Split('x')[1]);
+
+                for (int i = 0; i < width; i++)
+                {
+                    for (int j = 0; j < height; j++)
+                    {
+                        fabric[start + i, end + j]++;
+                    }
+                }
+            }
+        }
     }
 }
