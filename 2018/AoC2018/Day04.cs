8c1e11caea44222ed9afabab2baaa36e7b897897
Andrew Petz <AnPetz@ashleyfurniture.com>
2018-12-27 08:53:36 -0600

    Day 4

--- 2018/AoC2018/Day04.cs
+++ 2018/AoC2018/Day04.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AoC2018.Helpers.Day04;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,18 @@ namespace AoC2018
 {
     class Day04
     {
+        public static void PartOne()
+        {
+            List<string> input = Utilities.GetLineInput("04");
+
+            List<Guard> guards = new List<Guard>();
+
+            foreach (string s in input)
+            {
+
+            }
+
+            Console.ReadLine();
+        }
     }
 }
---
+++ 2018/AoC2018/Helpers/Day04.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AoC2018.Helpers.Day04
+{
+    class TimeEntry
+    {
+        public Guard Guard { get; set;}
+        public DateTime BeginsShift { get; set; }
+
+    }
+
+    class Guard
+    {
+        public int ID { get; set; }
+        public int SleepTime { get; set; }
+        public List<Time> Times { get; set; }
+    }
+
+    class Time
+    {
+        public int Minute { get; set; }
+        public int SleptTime { get; set; }
+    }
+}
