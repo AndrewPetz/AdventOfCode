e51023ae5527cdadfed01d661b016076e4510209
Andrew Petz <andrewwpetz@gmail.com>
2019-12-01 22:27:55 -0600

    Finished day 1

--- 2019/AoC2019/day1.cs
+++ 2019/AoC2019/day1.cs
@@ -15,9 +15,42 @@ namespace AoC2019
             {
                 // divide by three, round down, subtract 2
                 // add to total
-                total += Math.Floor(double.Parse(i) / 3) - 2;
+
+                // Part 1
+                //total += ProcessModule(double.Parse(i));
+
+                // Part 2
+                total += ProcessModulePt2(double.Parse(i));
             }
+
         }
-        
+
+        private static double ProcessModulePt2(double module)
+        {
+            // get fuel for current module
+            var curModule = ProcessModule(module);
+            var total = curModule;
+
+            // get fuel for that fuel until the amount required is zero
+            var newCurModule = 1.0;
+            while(newCurModule > 0)
+            {
+                newCurModule = ProcessModule(curModule);
+                if(newCurModule > 0)
+                {
+                    total += newCurModule;
+                    curModule = newCurModule;
+                }
+            }
+
+            
+
+            return total;
+        }
+
+        private static double ProcessModule(double module)
+        {
+            return Math.Floor(module / 3) - 2;
+        }
     }
 }
