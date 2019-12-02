61ad71a7db3cbe046e7bedcb6b35d497d89d9785
Andrew Petz <andrewwpetz@gmail.com>
2019-12-01 22:10:49 -0600

    Started 2019

---
+++ 2019/AoC2019/day1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AoC2019
+{
+    class Day1
+    {
+        public static void Run()
+        {
+            // get input
+            var input = Utils.GetLineInput("1");
+            var total = 0.0;
+
+            // solve problem
+            foreach(var i in input)
+            {
+                // divide by three, round down, subtract 2
+                // add to total
+                total += Math.Floor(double.Parse(i) / 3) - 2;
+            }
+        }
+        
+    }
+}
