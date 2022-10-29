89ffff014e04eae6e169be3c73fa0e6644522dbc
Andrew Petz <andrewwpetz@gmail.com>
2022-10-29 12:10:26 -0500

    Some stuff

--- 2015/Day04.cs
+++ 2015/Day04.cs
@@ -1,15 +1,53 @@
-﻿namespace _2015
+﻿using System.Security.Cryptography;
+using System.Text;
+using Utils;
+
+namespace _2015
 {
     public class Day04
     {
-        public static async Task Part1()
+        public static async Task<long> Part1()
         {
+            var input = await Files.ReadFileAsync(4);
+            long addon = 0;
+            var hash = "11111111111";
+            while (hash[..5] != "00000")
+            {
+                addon++;
+                var testNumber = input + addon;
+                hash = ComputeMd5Hash(testNumber);
+            }
 
+            return addon;
         }
 
-        public static async Task Part2()
+        public static async Task<long> Part2()
         {
+            var input = await Files.ReadFileAsync(4);
+            long addon = 0;
+            var hash = "11111111111";
+            while (hash[..6] != "000000")
+            {
+                addon++;
+                var testNumber = input + addon;
+                hash = ComputeMd5Hash(testNumber);
+            }
 
+            return addon;
+        }
+
+        public static string ComputeMd5Hash(string message)
+        {
+            using var md5 = MD5.Create();
+            var input = Encoding.ASCII.GetBytes(message);
+            var hash = md5.ComputeHash(input);
+
+            var sb = new StringBuilder();
+            foreach (var h in hash)
+            {
+                sb.Append(h.ToString("X2"));
+            }
+            return sb.ToString();
         }
     }
 }
--- 2015/Day06.cs
+++ 2015/Day06.cs
@@ -1,15 +1,64 @@
-﻿namespace _2015
+﻿using Utils;
+
+namespace _2015
 {
     public class Day06
     {
-        public static async Task Part1()
+        public static async Task<int> Part1()
         {
+            var input = await Files.ReadLinesAsync(6);
+            var lights = new bool[1000, 1000];
 
+            foreach (var i in input)
+            {
+                // parse instructions
+                switch (i.Substring(0, 7))
+                {
+                    case "turn on":
+                        TurnOn(lights, i);
+                        break;
+                    case "turn of":
+                        TurnOff(lights, i);
+                        break;
+                    case "toggle ":
+                        Toggle(lights, i);
+                        break;
+                }
+            }
+
+            var lightsOn = lights.Cast<bool>().Count(x => x);
+            return lightsOn;
         }
 
         public static async Task Part2()
         {
+            var input = await Files.ReadLinesAsync(6);
 
+        }
+
+        private static void TurnOn(ref bool[,] lights, string i)
+        {
+            var start = i.IndexOf(" ", 8) + 1;
+            var end = i.IndexOf(" through ");
+            var startCoords = i.Substring(start, end - start).Split(',').Select(int.Parse).ToArray();
+            start = end + 9;
+            end = i.Length;
+            var endCoords = i.Substring(start, end - start).Split(',').Select(int.Parse).ToArray();
+
+            for (int x = startCoords[0]; x <= endCoords[0]; x++)
+            {
+                for (int y = startCoords[1]; y <= endCoords[1]; y++)
+                {
+                    lights[x, y] = true;
+                }
+            }
+        }
+
+        private enum Instruction
+        {
+            TurnOn,
+            TurnOff,
+            Toggle
         }
     }
 }
